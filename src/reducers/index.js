efbde3a347b28e6dc44a3ca6b0dc1b286e5cb4d3
QinxinTian <aptsubl@gmail.com>
2018-08-19 00:00:16 -0700

    combineReducers.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,30 @@
+import { combineReducers } from 'redux'
 import {
+  //Normalization: Do not duplicate data;
+  //Keep your store as shallow as possible.
   ADD_RECIPE,
   REMOVE_FROM_CALENDAR,
+  /*The problem is that Redux’s createStore() method
+  takes in a single reducer, not multiple.
+  To combine all of your reducers into one, you
+  can use Redux’s combineReducers() method.*/
 } from '../actions'
+////create the reducer for food, so whenever the ADD_RECIPE action
+//is dispatched, not only modify the calendar state
+//but also modify the food section of store
+function food (state = {}, action) {
+  switch (action.type) {
+    case ADD_RECIPE :
+      const { recipe } = action
+
+      return {
+        ...state,
+        [recipe.label]: recipe,
+      }
+    default :
+      return state
+  }
+}
 
 const initialCalendarState = {
   sunday: {
@@ -65,5 +88,17 @@ function calendar (state = initialCalendarState, action) {
       return state
   }
 }
+/*import { combineReducers } from 'redux';
+import booksReducer from './books_reducer';
+import userReducer from './user_reducer';
 
-export default calendar
+const rootReducer = combineReducers({
+    books: booksReducer,
+    users: userReducer
+});
+
+export default rootReducer;*/
+export default combineReducers({
+  food,
+  calendar,
+})
